2f20d38217a69048694271ae69f20c3efbc9d97c
Siddharth-here <siddharthmohapatra2004@gmail.com>
2026-06-25 20:48:01 +0530

    custom validator created

---
+++ src/common/middleware/validate.middleware.js
@@ -0,0 +1,17 @@
+import ApiError from "../utils/api-error.js"
+
+//custom validator
+const validate = (Dtoclass) =>{
+    return (req, res, next) =>{
+        const {error, value} = Dtoclass.validate(req.body)
+
+        if(errors){
+            throw ApiError.badRequest(errors.join(";"))
+        }
+
+        req.body = value
+        next()
+    }
+
+}
+export default validate
